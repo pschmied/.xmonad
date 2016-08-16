fec356dabdcbcb5e26ec0ab69a2f7bcaca8d7034
Peter Schmiedeskamp <peter@thoughtspot.net>
2016-08-16 14:37:56 -0700

    cleaner, more minimalist

--- xmonad.hs
+++ xmonad.hs
@@ -1,21 +1,18 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
+import XMonad.Config.Desktop
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+
+baseConfig = desktopConfig
 
 main = do
 
-xmproc <- spawnPipe "/usr/local/bin/xautolock -time 15 -locker slock"
-xmproc <- spawnPipe "/usr/X11R6/bin/xsetroot -solid gray"
-xmproc <- spawnPipe "/usr/local/bin/xmobar ~/.xmobarrc"
-xmproc <- spawnPipe "/usr/X11R6/bin/setxkbmap -option ctrl:nocaps -option compose:ralt"
-xmproc <- spawnPipe "/usr/bin/mixerctl  outputs.master=156"
+  xmproc <- spawnPipe "/usr/local/bin/xautolock -time 15 -locker slock"
+  xmproc <- spawnPipe "/usr/X11R6/bin/xsetroot -solid lightblue"
+  -- xmproc <- spawnPipe "/usr/local/bin/xmobar ~/.xmobarrc"
+  xmproc <- spawnPipe "/usr/X11R6/bin/setxkbmap -option ctrl:nocaps -option compose:ralt"
+  xmproc <- spawnPipe "/usr/bin/mixerctl  outputs.master=156"
 
-xmonad $ defaultConfig
-           {modMask = mod4Mask
-           , terminal = "urxvt +sb"
-           , manageHook = manageDocks <+> manageHook defaultConfig
-           , layoutHook = avoidStruts  $  layoutHook defaultConfig
-           }
+  xmonad $ baseConfig
+    { modMask = mod4Mask
+    , terminal = "urxvt +sb"
+    }
